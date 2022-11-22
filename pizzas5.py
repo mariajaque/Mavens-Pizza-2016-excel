ecb8c5084874ddc8d1b57138100f8258cd111c87
Maria <202110874@alu.comillas.edu>
2022-11-22 22:09:26 +0100

    Version 2

--- pizzas5.py
+++ pizzas5.py
@@ -10,12 +10,6 @@ the size and pizza type
 """
 
 import pandas as pd
-import numpy as np
-import re
-import xml.etree.ElementTree as ET
-from fpdf import FPDF
-import matplotlib.pyplot as plt
-import seaborn as sns
 from openpyxl import load_workbook
 from openpyxl.chart import BarChart, Reference
 from openpyxl.styles import Font, PatternFill
